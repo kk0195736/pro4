3944a82dfa92d83ebafc038788a0e8d7b98c92f5
kk0195736 <kk094803>
2018-11-14 13:55:23 +0900

    test

--- dataset-parser.py
+++ dataset-parser.py
@@ -1,13 +1,12 @@
 # 1. read dataset
 # 2. parse dataset
 
-# test
-
-
 def read_dataset(path):
-    for line in f:
-        
-        pass
+        f = open(path)
+        for line in f:
+                if line.find('GET') == 0 or if line.find('POST') == 0:
+                        string 
+        f.close()
 
 if __name__ == '__main__':
-read_dataset('./static/')
+read_dataset('./static/normalTrafficTraining.txt')
