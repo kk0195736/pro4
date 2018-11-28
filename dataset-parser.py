10a9cce5cc2ed178f60709d0b9be79898ada20bd
kk0195736 <kk019573611296911@gmail.com>
2018-11-28 12:06:53 +0900

    update

--- dataset-parser.py
+++ dataset-parser.py
@@ -1,6 +1,6 @@
 # 1. read dataset
 # 2. parse dataset
-
+'''
 def read_dataset(path):
         f = open(path)
         for line in f:
@@ -10,3 +10,25 @@ def read_dataset(path):
 
 if __name__ == '__main__':
 read_dataset('./static/normalTrafficTraining.txt')
+'''
+
+
+def parse_dataset(filepath):
+    text = ''
+    with open(filepath) as f:
+        for line in f:
+            if ('GET' in line) or ('POST' in line) or ('PUT' in line):
+                if text != '':
+                    yield text
+                    text = ''
+            text = text + line
+        yield text
+
+
+N = []
+
+if __name__ == "__main__":
+
+    N.append(parse_dataset("./static/normalTrafficTraining.txt"))
+
+    print(N)
